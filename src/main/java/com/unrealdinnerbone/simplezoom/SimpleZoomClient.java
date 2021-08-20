fd300156c1ca36b2c4ed94c252e7ba7c8f22dc6c
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2021-08-19 20:40:45 -0500

    Fixed Mappings change

--- src/main/java/com/unrealdinnerbone/simplezoom/SimpleZoomClient.java
+++ src/main/java/com/unrealdinnerbone/simplezoom/SimpleZoomClient.java
@@ -1,31 +1,23 @@
 package com.unrealdinnerbone.simplezoom;
 
-import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.api.distmarker.Dist;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
-import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import org.lwjgl.glfw.GLFW;
-
-import java.nio.file.Paths;
 
 public class SimpleZoomClient {
 
     private final SimpleZoom simpleZoom;
-    private final KeyMapping zoomBind;
+    private final KeyBinding zoomBind;
 
     private static boolean isSmoothCameraOn = false;
     private static double zoomCount = 0.0f;
 
     public SimpleZoomClient(SimpleZoom simpleZoom) {
         this.simpleZoom = simpleZoom;
-        zoomBind = new KeyMapping(SimpleZoom.MOD_ID + "." + "zoom", GLFW.GLFW_KEY_Z, SimpleZoom.MOD_ID + "." + "zoom");
+        zoomBind = new KeyBinding(SimpleZoom.MOD_ID + "." + "zoom", GLFW.GLFW_KEY_Z, SimpleZoom.MOD_ID + "." + "zoom");
         ClientRegistry.registerKeyBinding(zoomBind);
         MinecraftForge.EVENT_BUS.addListener(this::onFOVModifierEvent);
     }
